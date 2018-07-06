2fb35947f2015637528e037e49a1db5b29239e12
Timmy Wertz <timmylwertz@gmail.com>
2018-07-06 15:29:36 -0400

    image added

--- src/app.js
+++ src/app.js
@@ -1,42 +1,7 @@
-// import React from 'react'
-// import fortuneCookie from 'fortune-cookie'
-//
-// function getRandomInt(max) {
-// 	return Math.floor(Math.random() * Math.floor(max))
-// }
-//
-// const App = props => {
-// 	const state = props.State
-// 	const setState = props.setState
-//
-// 	const getFortune = () => {
-// 		const number = getRandomInt(fortuneCookie.length - 1)
-// 		return {
-// 			fortune: fortuneCookie[number]
-// 		}
-// 	}
-//
-// 	return (
-// 		<div>
-// 			<h1>Fortune Cookie App</h1>
-// 			<h3>{state.fortune}</h3>
-// 			<button
-// 				onClick={() => {
-// 					setState(getFortune)
-// 				}}
-// 			>
-// 				Get Fortune
-// 			</button>
-// 			<p>Click button to get fortune</p>
-// 		</div>
-// 	)
-// }
-//
-// export default App
-
 import React from 'react'
 import fortuneCookie from 'fortune-cookie'
 import style from './app.css.js'
+import logo from './FortuneCookieIMG.png'
 
 function getRandomInt(max) {
 	return Math.floor(Math.random() * Math.floor(max))
@@ -55,17 +20,28 @@ const App = props => {
 
 	return (
 		<div style={style.root}>
+			<img height="100" src={logo} alt="fortune cookie logo" />
 			<h1>Fortune Cookie App</h1>
-			<h3 className="mh2 fw1 ba pa4 br2 bg-light-blue">{state.fortune}</h3>
-			<button
-				style={style.button}
-				onClick={() => {
-					setState(getFortune)
+			<div
+				style={{
+					display: 'flex',
+					flex: 1,
+					alignItems: 'center',
+					justifyContent: 'center',
+					flexDirection: 'column'
 				}}
 			>
-				Get Fortune
-			</button>
-			<p className="instructions">Click button to get fortune</p>
+				<h3 className="mh2 fw1 ba pa4 br2 bg-light-blue">{state.fortune}</h3>
+				<button
+					style={style.button}
+					onClick={() => {
+						setState(getFortune)
+					}}
+				>
+					Get Fortune
+				</button>
+				<p className="instructions">Click button to get fortune</p>
+			</div>
 		</div>
 	)
 }
